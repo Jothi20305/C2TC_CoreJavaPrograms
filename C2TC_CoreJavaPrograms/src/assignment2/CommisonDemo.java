bfcfe2a37b5359656fa68c0e45e502b4d8ec875e
Asus <Asus@DESKTOP-5O1UGJK>
2025-07-23 21:03:52 +0530

    This is Assignment 2

---
+++ C2TC_CoreJavaPrograms/src/assignment2/CommisonDemo.java
@@ -0,0 +1,10 @@
+package assignment2;
+
+public class CommisonDemo {
+	public static void main(String[] args) {
+		Student s = new Student();
+        Commision c = new Commision();
+        c.acceptDetails();
+        c.calculateCommission();
+	}
+}
