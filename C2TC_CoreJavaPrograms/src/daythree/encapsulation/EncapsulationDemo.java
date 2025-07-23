9d6614a680d842be6914e5893c8a7153f3a50c78
Asus <Asus@DESKTOP-5O1UGJK>
2025-07-23 22:34:32 +0530

    This is DayThree work

---
+++ C2TC_CoreJavaPrograms/src/daythree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Vasant");
+		obj.setAge(23);
+		System.out.println(obj);
+
+	}
+
+}
