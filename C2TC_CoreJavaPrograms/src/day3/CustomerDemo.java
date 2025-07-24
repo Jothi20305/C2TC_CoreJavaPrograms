fce11216799d2c5cf5872302e819595237243aef
Asus <Asus@DESKTOP-5O1UGJK>
2025-07-24 14:20:18 +0530

    This is Day Four Assignment

---
+++ C2TC_CoreJavaPrograms/src/day3/CustomerDemo.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		System.out.println(c1);
+      Customer c2=new Customer(102,"Jagan","Chennai");
+      System.out.println(c2);
+	}
+}
