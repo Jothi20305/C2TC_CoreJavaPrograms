3a2db4852102a1788174d4273d91918e744cf437
Asus <Asus@DESKTOP-5O1UGJK>
2025-07-24 23:32:56 +0530

    MultiLevel Inheritance

---
+++ C2TC_CoreJavaPrograms/src/dayfive/multileveinheritance/vehicle/Car.java
@@ -0,0 +1,11 @@
+package dayfive.multileveinheritance.vehicle;
+
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+}
