d23e6275aee4fb37d58ec91f9784604013dcde45
Asus <Asus@DESKTOP-5O1UGJK>
2025-07-29 22:35:35 +0530

    This is DMART Assignment

---
+++ C2TC_CoreJavaPrograms/src/dmart/Trends.java
@@ -0,0 +1,9 @@
+package dmart;
+
+public class Trends implements Dmart{
+	@Override
+    public void displayItems() {
+        System.out.println("Welcome to the Trends Section!");
+        System.out.println("Available items: Shirts, Jeans, Dresses, Footwear");
+    }
+}
