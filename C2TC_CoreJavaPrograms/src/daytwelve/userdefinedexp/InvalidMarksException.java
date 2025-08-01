63ceefcb80c609a9874d9fc7ceb41358d321f8a2
Asus <Asus@DESKTOP-5O1UGJK>
2025-08-01 21:45:21 +0530

    Day 12 work

---
+++ C2TC_CoreJavaPrograms/src/daytwelve/userdefinedexp/InvalidMarksException.java
@@ -0,0 +1,11 @@
+package daytwelve.userdefinedexp;
+
+public class InvalidMarksException extends Exception{
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+}
