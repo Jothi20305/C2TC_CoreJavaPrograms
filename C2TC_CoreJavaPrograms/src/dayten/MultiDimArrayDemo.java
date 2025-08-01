c97107f274bc505c78de30b8d708fcad49f07766
Asus <Asus@DESKTOP-5O1UGJK>
2025-08-01 21:19:14 +0530

    Day 10

--- C2TC_CoreJavaPrograms/src/dayten/MultiDimArrayDemo.java
+++ C2TC_CoreJavaPrograms/src/dayten/MultiDimArrayDemo.java
@@ -6,9 +6,8 @@ public class MultiDimArrayDemo {
 		int c[][] = { { 12, 45 }, { 10, 40 }, { 5, 7 } }; // new int[3][2];
 
 		System.out.println("No. of Rows in C array : " + c.length);
-
+		
 		MLArray.printArray(c);
-
 	}
 
 }
